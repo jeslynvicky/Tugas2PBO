1af1d643f75363c555a656fa8ecc2bebde48a20e
jeslynvicky <162588624+jeslynvicky@users.noreply.github.com>
2024-03-16 11:33:21 +0700

    Add files via upload

---
+++ Tugas2/src/bangundatar/OBD.java
@@ -0,0 +1,8 @@
+package bangundatar;
+
+public interface OBD {
+    public double Luas();
+    public double Keliling();
+    public double Volume();
+    public double LuasPermukaan();
+}
---
+++ Tugas2/src/tugas2/Tugas2.java
@@ -0,0 +1,9 @@
+package tugas2;
+
+public class Tugas2 {
+    public static void main(String[] args) {
+        // Mulai dari halaman login
+        new login();
+    }
+    
+}
